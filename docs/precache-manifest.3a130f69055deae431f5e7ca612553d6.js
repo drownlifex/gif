b8accb5c75202dbea1ebec22f2d1ab1c497b883d
Jack <drownlifex@gmail.com>
2020-08-09 11:11:46 -0300

    dock agregado

---
+++ docs/precache-manifest.3a130f69055deae431f5e7ca612553d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1082ab3765a1a4ca0f62d18a8f1820d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cb54a6197fba335d20e",
+    "url": "/static/css/main.9a7b9b01.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cb54a6197fba335d20e",
+    "url": "/static/js/main.bd309cca.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
